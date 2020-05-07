2cb07eae497de60fab373c74fe2cdca51743171e
ddombrow <daniel@socialtables.com>
2020-05-07 11:11:19 -0400

    add intersection to exercise 1

--- exercises/e01-union-and-intersection-types/e01.ts
+++ exercises/e01-union-and-intersection-types/e01.ts
@@ -2,8 +2,7 @@
 Intro:
 	So let's talk about the shapes we want to draw on these cards
 Exercise:
-	Type "Shape" is missing, please define it and use
-	it in shapes array in the test.
+	Type "Shape" is missing, please try defining is as as an intersection type and then a union type.
 */
 
 export enum ShapeKind {
@@ -22,10 +21,14 @@ interface Circle {
 	radius: number;
 }
 
-// Tips and tricks
+// Tips and notes
 // note that in more recent versions of typescript you can export and import types as we are doing here with ShapeKind
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#-type-only-imports-and-export
 // when would you use interface and when would you use type https://stackoverflow.com/questions/37233735/typescript-interfaces-vs-types
 
+// is intersection or union the best for our shape type???
+// can we use an intersection type to combin the kind with the other shape properties?
+
 // Reading:
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html#union-types
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#intersection-types
