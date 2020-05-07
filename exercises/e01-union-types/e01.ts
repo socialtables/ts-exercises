df6bc5cb9af7f3e3aac315deb067c16e4b9371ff
ddombrow <daniel@socialtables.com>
2020-05-07 11:00:34 -0400

    more tips for exercise 1

--- exercises/e01-union-types/e01.ts
+++ exercises/e01-union-types/e01.ts
@@ -24,6 +24,8 @@ interface Circle {
 
 // Tips and tricks
 // note that in more recent versions of typescript you can export and import types as we are doing here with ShapeKind
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#-type-only-imports-and-export
+// when would you use interface and when would you use type https://stackoverflow.com/questions/37233735/typescript-interfaces-vs-types
 
 // Reading:
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html#union-types
