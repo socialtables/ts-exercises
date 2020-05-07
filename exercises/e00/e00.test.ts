b3ca742b00c083cd3e43a2b8a04202351fcc422f
ddombrow <daniel@socialtables.com>
2020-05-07 09:50:52 -0400

    eslint config and exercise 0

---
+++ exercises/e00/e00.test.ts
@@ -0,0 +1,12 @@
+import "./e00.ts";
+
+describe("exercise 00 tests", () => {
+	test("should be able to define some cards", () => {
+		const cards: unknown[] = [
+			{ x: 2, y: 2, hidden: true, kind: "rectangle" },
+			{ x: 3, y: 3, hidden: true, kind: "diamond" }
+		];
+		expect(cards).toBeDefined()
+		expect(cards[0]).toHaveProperty("x");
+	});
+})
---
+++ exercises/e00/e00.ts
@@ -0,0 +1,15 @@
+/*
+Intro:
+	We are starting a simple memory game program for kids.  There will be cards laid out in
+	a 3x4 grid, each having a shape drawn on it.
+Exercise:
+	1. Given the data defined in the test file, define the interface "Card" and use it accordingly.
+*/
+
+interface Card {
+	something?: unknown;
+}
+
+enum ShapeKinds {
+	Default
+}
