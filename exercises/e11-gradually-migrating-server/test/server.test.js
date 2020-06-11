438739d82cee5ee556260b78833e6903ff3d0806
ddombrow <daniel@socialtables.com>
2020-06-11 13:10:26 -0400

    mo tests, change package name

--- exercises/e11-gradually-migrating-server/test/server.test.js
+++ exercises/e11-gradually-migrating-server/test/server.test.js
@@ -22,4 +22,15 @@ describe('test the server', () => {
     const res = await axios.get(`${baseUrl}/health-check`);
     expect(res.status).toEqual(200);
   });
+  test('adding should work', async () => {
+    const res = await axios.get(`${baseUrl}/add?a=1&b=1`);
+    expect(res.status).toEqual(200);
+    expect(res.data).toEqual('2');
+  });
+  test('expressions should work', async () => {
+    const expression = encodeURIComponent('12.7 cm to inch');
+    const res = await axios.get(`${baseUrl}/calc?expr=${expression}`);
+    expect(res.status).toEqual(200);
+    expect(res.data).toEqual('5 inch');
+  });
 });
